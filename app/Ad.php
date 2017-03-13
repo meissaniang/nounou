0b73c6b06e10b30fb0628477f6d37705a5477898
meissa <meissa.niang.94@gmail.com>
2017-03-13 15:19:46 +0000

    ad

--- app/Ad.php
+++ app/Ad.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Ad extends Model
 {
     protected $table = 'ads';
-    protected $fillable = ['title', 'ad', 'frequence','occasional_price','full_time_price','nanny_id'];
+    protected $fillable =  ['title', 'ad', 'frequence','occasional_price','full_time_price','nanny_id'];
     public function nanny(){
         return $this->belongsTo('App\Nanny');
     }
